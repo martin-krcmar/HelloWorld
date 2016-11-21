5402d466f7867c772e9011377b0307d1827b2283
martin-krcmar <martin.krcmar@atlas.cz>
2016-11-21 14:24:51 +0100

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,11 @@
 var http = require('http');
+
+// testing PRs
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'test/plain'});
 	res.end('Hello World\n');
 }).listen(8124);
+
 console.log('Server running at http://localhost:8124/');
 
 
